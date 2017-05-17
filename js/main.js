efcbbc15cd292c24fb1372a0a102331baa5e4937
sdkevorkian <sdkevorkian@gmail.com>
2017-05-17 16:16:44 -0700

    nav not as jumpy, gonna move to smooth page jumps now

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
   $(document).ready(function() {
       $('#pin-this-nav').each(function() {
           var $this = $(this);
-          var $target = $('#' + $(this).attr('data-target'));
           $this.pushpin({
-              top: $target.offset().top + $target.outerHeight() + 18
+              top: $this.offset().top
           });
       });
+
+
+
   });
