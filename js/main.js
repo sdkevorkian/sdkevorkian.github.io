7927e4197b4292c0f265f13c7022adedf011f775
sdkevorkian <sdkevorkian@gmail.com>
2017-05-17 16:58:17 -0700

    fixed some margins and added smooth scroll

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
   $(document).ready(function() {
-      $('#pin-this-nav').each(function() {
-          var $this = $(this);
-          $this.pushpin({
-              top: $this.offset().top
-          });
+      $('#pin-this-nav').pushpin({
+          // when you scroll past the offset of the nav from the top, then fix nav bar
+          top: $('#pin-this-nav').offset().top
       });
 
+      $("a").on('click', function(event) {
 
-
+          // Make sure this.hash has a value before overriding default behavior
+          if (this.hash !== "") {
+              // Prevent default anchor click behavior
+              event.preventDefault();
+              // Store hash (ID tag to navigate to)
+              var hash = this.hash;
+              // Using jQuery's animate() method to add smooth page scroll
+              // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+              $('html, body').animate({
+                  scrollTop: $(hash).offset().top
+              }, 1000, function() {
+                  // Add hash (#) to URL when done scrolling (default click behavior)
+                  window.location.hash = hash;
+              });
+          }
+      });
   });
