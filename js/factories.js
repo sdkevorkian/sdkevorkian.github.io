695184c52c3e3b7f47c6c03706ec25aeb5da3858
sdkevorkian <sdkevorkian@gmail.com>
2017-06-07 21:12:48 -0700

    playing with factories

--- js/factories.js
+++ js/factories.js
@@ -2,7 +2,13 @@ angular.module('WorkApp', [])
     .factory('WorkFactory', function() {
         return {
             getProjects: function() {
-                return [{}]; // put array of projects here
+                return [{
+                    name: "Browsergotchi",
+                    technologies: ['HTML5', 'CSS3', 'JavaScript', 'JQuery', 'GIMP'],
+                    github: 'https://github.com/sdkevorkian/browsergotchi',
+                    liveSite: 'https://sdkevorkian.github.io/browsergotchi/',
+                    imgUrl: 'images/work/browsergotchi.png'
+                }]; // put array of projects here
             }
         };
     });
