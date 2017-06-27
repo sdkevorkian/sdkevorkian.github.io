70d0c8c3e926ab929e4c847182399416d7184ae0
sdkevorkian <sdkevorkian@gmail.com>
2017-06-26 18:28:16 -0700

    made rearrangements. need to restyle skills

--- js/factories.js
+++ js/factories.js
@@ -2,6 +2,7 @@ angular.module('WorkApp', [])
     .factory('WorkFactory', function() {
         return {
             getProjects: function() {
+                var contributors = [{ name: "Alaina Buzas", link: "http://www.alainabuzas.com" }, { name: "Andrew Tsujimoto", link: "http://www.andrewtsujimoto.com" }, { name: "Mark Goldstein", link: "http://markdgold.github.io" }, { name: "Peter Amadon" }, { name: "Ben Frates (UX/UI)", link: "https://www.benfrates.com/" }];
                 return [{
                     name: "Doggleganger",
                     technologies: "HTML5, CSS3, JavaScript, Angular.js, Node.js, Express.js, MongoDB, JWT, Petfinder API, GraphicsMagick\'s npm module",
@@ -9,7 +10,7 @@ angular.module('WorkApp', [])
                     liveSite: 'https://doggleganger.herokuapp.com/',
                     imgUrl: 'images/work/doggleganger.png',
                     description: "Doggleganger allows you to search for the perfect pet based on your percentage look-alike match. This was a three-person group project, and I was primarily responsible for the image comparison features, Angular.js implementation, troubleshooting, and deployment.",
-                    contributors: [{ name: "Alaina Buzas", link: "http://www.alainabuzas.com" }, { name: "Andrew Tsujimoto", link: "http://www.andrewtsujimoto.com" }]
+                    contributors: [contributors[0], contributors[1]]
                 }, {
                     name: "Chronotron",
                     technologies: "HTML5, CSS3, JavaScript, JQuery, Ruby on Rails, Youtube API, Trails API, Spotify API, Open Trivia API, Open Weather API, Google Analytics",
@@ -17,7 +18,7 @@ angular.module('WorkApp', [])
                     liveSite: 'https://chronotron.herokuapp.com/',
                     imgUrl: 'images/work/chronotron.png',
                     description: "“There's never enough time to do all the nothing you want.” Give Chronotron an amount of time you need to spend and it will provide you enough content to fill that void. This project was created by a 5 person dev team working with 1 UX/UI Consultant. My responsibilities were the music section, the homepage and side navbar logic, deployment, troubleshooting, and implementing suggestions from the UX/UI consultant.",
-                    contributors: [{ name: "Alaina Buzas", link: "http://www.alainabuzas.com" }, { name: "Andrew Tsujimoto", link: "http://www.andrewtsujimoto.com" }, { name: "Mark Goldstein", link: "http://markdgold.github.io" }, { name: "Peter Amadon" }, { name: "Ben Frates (UX/UI)", link: "https://www.benfrates.com/" }]
+                    contributors: [contributors[0], contributors[1], contributors[2], contributors[3], contributors[4], ]
 
                 }, {
                     name: "Browsergotchi",
@@ -40,7 +41,7 @@ angular.module('WorkApp', [])
                     liveSite: 'https://onesnoozeaway.herokuapp.com/',
                     imgUrl: "images/work/onesnoozeaway.png",
                     description: "One Snooze Away is here to make sure you never sleep through your stop again. Simply search for your destination, set an alarm, and take a nap. We'll wake you up when the bus is approaching your stop. One Snooze Away was created by two devs as a part of a one-day,  MEAN Hackathon. I took on a back-end role and handled the Node.js/Express.js routing, MongoDB implementation, and Angular.js logic.",
-                    contributors: [{ name: "Alaina Buzas", link: "http://www.alainabuzas.com" }]
+                    contributors: [contributors[0]]
                 }];
             }
         };
