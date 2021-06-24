baa7ed9125af51b7b5d34265978a170d0e4a7dec
Sara Kevorkian <sdkevorkian@gmail.com>
2021-06-24 12:55:26 -0700

    first start on nextjs version

---
+++ portfolio/pages/work/first-work.js
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../../components/layout'
+
+export default function FirstWork() {
+    return (
+        <Layout>
+            <Head>
+                <title>First Work</title>
+            </Head>
+            <h1>
+                First work
+            </h1>
+            <Link href="/">
+            <a>Back to home</a>
+            </Link>
+        </Layout>
+        )
+  }
