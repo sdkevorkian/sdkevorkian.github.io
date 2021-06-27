39e99c79002c2e64f813b2a8763434f5bbcbde51
Sara Kevorkian <sdkevorkian@gmail.com>
2021-06-26 23:00:41 -0700

    nav formatting

--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -30,7 +30,7 @@ export default function Home({allPostsData}) {
           alt='Sara Kevorkian'
         />
         <h1>Sara Kevorkian</h1>
-        <h2>Web Developer  Seattle, WA</h2>
+        <h2>Web Developer Seattle, WA</h2>
         <p>Hi, I'm Sara!
 
         I grew up near Seattle and enjoy living in the PNW. I am a Seattle-based front end developer that has delivered for a variety of industries including airlines and real estate. I am primarily a front end developer with some back end development experience. My background in biotech taught me the importance of flexibility and the mindset to break concepts down into testable pieces. I am an analytical thinker and love to ask questions, while keeping a collaborative, positive mindset. In my 2+ years of experience I have learned new technologies quickly to meet project needs.</p>
