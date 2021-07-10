73979d961a1f89f3160969d471d4e7ed655dd461
Sara Kevorkian <sdkevorkian@gmail.com>
2021-07-10 16:39:20 -0700

    move resume into contact

---
+++ portfolio/components/resume/resume.js
@@ -0,0 +1,19 @@
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import resumeStyles from './resume.module.scss';
+
+export default function Resume(props){
+    let resume = props.resume;
+    return (<div>
+        <h2>Resume</h2>
+        {documentToReactComponents(resume.text)}
+        <div>
+            
+            <a href={resume.pdfResume.fields.file.url} download>Download PDF</a>
+            <a href={resume.pdfResume.fields.file.url} target="_blank">View PDF</a>
+        </div>
+        <div>
+            <a href={resume.pdfResume.fields.file.url} download>Download Plain Text</a>
+            <a href={resume.plainTextResume.fields.file.url} target="_blank">View Plain Text</a>
+        </div>
+    </div>)
+}
--- portfolio/pages/contact.js
+++ portfolio/pages/contact.js
@@ -1,15 +1,30 @@
 import Head from 'next/head'
 import Layout from '../components/layout/layout'
+import Resume from '../components/resume/resume'
+import { getNav, getResume } from '../lib/CMS'
 
-export default function Contact(){
+export async function getStaticProps({ params }) {
+    const nav = await getNav()
+    const resume = await getResume();
+    return {
+        props: {
+        nav,
+        resume
+        }
+    }
+}
+
+
+export default function Contact({nav, resume}){
     return (
-        <Layout>
+        <Layout nav={nav}>
             <Head>
                 <title>Contact Sara Kevorkian</title>
             </Head>
             <section>
                 <h1>Contact</h1>
             </section>
+            <Resume resume={resume}/>
         </Layout>
     )
 }
