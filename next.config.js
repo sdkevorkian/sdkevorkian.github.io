d108b678a7ab2b2dc238f0dfa61f791438d90513
Sara Kevorkian <sdkevorkian@gmail.com>
2021-07-17 13:53:46 -0700

    try removing basepath

--- next.config.js
+++ next.config.js
@@ -2,6 +2,4 @@ module.exports = {
     images: {
       domains: ['images.ctfassets.net'],
     },
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
   }
