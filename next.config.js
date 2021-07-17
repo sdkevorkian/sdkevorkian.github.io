e410899247d81442a42388dfbf8501b01610e434
Sara Kevorkian <sdkevorkian@gmail.com>
2021-07-17 12:45:31 -0700

    add env variables

--- next.config.js
+++ next.config.js
@@ -2,4 +2,6 @@ module.exports = {
     images: {
       domains: ['images.ctfassets.net'],
     },
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
   }
