88b98df4231c6fe017561c62ae49dfaf660be2bb
Sara Kevorkian <sdkevorkian@gmail.com>
2021-07-17 12:11:27 -0700

    delete old version of portfolio

--- old-version/js/app.js
+++
@@ -1,8 +0,0 @@
-angular.module('Portfolio', ['WorkApp'])
-    .controller('WorkCtrl', ['$scope', 'WorkFactory', function($scope, WorkFactory) {
-
-        $scope.projects = WorkFactory.getProjects();
-
-
-
-    }]);
